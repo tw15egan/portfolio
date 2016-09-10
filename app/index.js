9a0ce34be40881d61a4ac07826ec34da6879587b
TJ Egan <twegan@us.ibm.com>
2016-09-09 22:01:22 -0500

    initial commit

---
+++ app/index.js
@@ -0,0 +1,19 @@
+require('./scss/main.scss');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);  
+  }
+  
+  render() {
+    return (
+      <h1>TJ EGAN</h1>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+  
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const validate = require('webpack-validator');
+
+// HMR Config
+const parts = require('./libs/parts.js');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+}
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    // publicPath: '/color-picker/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'TJ Egan',
+      template: 'app/index.html'
+    })
+  ]
+};
+
+var config;
+
+switch(process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(
+      common, 
+      {
+        devtool: 'source-map'
+      }
+    );
+    break;
+  default:
+    config = merge(
+      common, 
+      {
+        devtool: 'eval-source-map'
+      },
+      parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+  );
+}
+
+module.exports = validate(config);
