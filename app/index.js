521aea77a6ffa5e2918df26d516b6d1295941a7a
TJ Egan <twegan@us.ibm.com>
2016-09-09 23:43:07 -0500

    blah

--- app/index.js
+++ app/index.js
@@ -3,14 +3,21 @@ require('./scss/main.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const posterBg = require('./img/4.jpeg');
+
 class App extends React.Component {
   constructor(props) {
     super(props);  
   }
   
   render() {
+    const divStyle = {
+      backgroundImage: `url(${posterBg})`
+    }
     return (
-      <h1>TJ EGAN</h1>
+      <div className="container" style={divStyle}>
+        <h1>TJ EGAN</h1>
+      </div>
     )
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,13 @@ const common = {
       {
         test: /\.scss$/,
         loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       }
     ]
   },
