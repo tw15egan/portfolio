1ab12982b0e1b4016f314fe8077590ac1c9ffb7a
TJ Egan <twegan@us.ibm.com>
2016-09-30 20:36:20 -0500

    add icon intro animation, more refactoring

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import SocialIcons from './social-icons';
+import tj from '../img/tj.jpg';
+
+const divStyle = {
+  backgroundImage: `url(${tj})`,
+};
 
 function Hero() {
   return (
-    <div className="hero">
-      <div className="hero__block hero__block--title">
-        <h1 className="hero__title">TJ&nbsp; EGAN</h1>
+    <div>
+      <div className="overlay">
+        <div className="overlay__image" style={divStyle} />
       </div>
-      <div className="hero__block hero__block--content">
-        <p className="hero__content">Front End Developer + Designer based in <a className="hero__content--link" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Austin,+TX/@30.3079828,-97.8934872,11z/data=!3m1!4b1!4m5!3m4!1s0x8644b599a0cc032f:0x5d9b464bd469d57a!8m2!3d30.2671524!4d-97.74306">Austin, TX</a></p>
+      <div className="hero">
+        <div className="hero__block hero__block--title">
+          <h1 className="hero__title">TJ&nbsp; EGAN</h1>
+        </div>
+        <SocialIcons />
+        <div className="hero__block hero__block--content">
+          <p className="hero__content">Front End Developer + Designer based in <a className="hero__content--link" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Austin,+TX/@30.3079828,-97.8934872,11z/data=!3m1!4b1!4m5!3m4!1s0x8644b599a0cc032f:0x5d9b464bd469d57a!8m2!3d30.2671524!4d-97.74306">Austin, TX</a></p>
+        </div>
       </div>
-      <SocialIcons />
     </div>
   );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hero from './components/hero';
-import tj from './img/tj.jpg';
 
 require('./scss/main.scss');
-
-const divStyle = {
-  backgroundImage: `url(${tj})`,
-};
 
 function App() {
   return (
     <div className="container">
-      <div className="overlay">
-        <div className="overlay__image" style={divStyle} />
-      </div>
       <Hero />
       <div className="portfolio" />
     </div>
