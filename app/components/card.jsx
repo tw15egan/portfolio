1fb36b1aa74a206a1a86e09a28e655044d65118c
TJ Egan <twegan@us.ibm.com>
2016-10-03 21:02:33 -0500

    add portfolio interactions

--- app/components/card.jsx
+++ app/components/card.jsx
@@ -4,19 +4,40 @@ export default class Card extends React.Component {
 
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
 
     this.state = {
       img: '' || this.props.img,
       project: '' || this.props.project,
       url: '' || this.props.url,
+      active: true,
     };
   }
+
+  handleClick(evt) {
+    evt.preventDefault();
+    this.toggleActive(evt);
+  }
+
+  toggleActive(evt) {
+    evt.preventDefault();
+    this.setState({
+      active: !this.state.active,
+    });
+  }
+
   render() {
+    const containerClass = this.state.active ? 'card__container card__container--active' : 'card__container';
     return (
       <div className="card">
-        <p>{this.state.url}</p>
-        <p>{this.state.project}</p>
-        <p>{this.state.img}</p>
+        <a href="/" onClick={this.handleClick} className="card__handle">
+          {this.state.project}
+        </a>
+        <div className={containerClass}>
+          <p>{this.state.url}</p>
+          <p>{this.state.project}</p>
+          <p>{this.state.img}</p>
+        </div>
       </div>
     );
   }
--- app/components/portfolio.jsx
+++ app/components/portfolio.jsx
@@ -4,6 +4,7 @@ import Card from './card';
 function Portfolio() {
   return (
     <div className="portfolio">
+      <h1 className="portfolio__title">Recent Work</h1>
       <Card url="www.google.com" img="../img/tj.jpg" project="Rapid Tow Bar" />
       <Card url="www.google.com" img="../img/tj.jpg" project="Rapid Tow Bar" />
       <Card url="www.google.com" img="../img/tj.jpg" project="Rapid Tow Bar" />
