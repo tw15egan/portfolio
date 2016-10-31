0068aaae5fec419136216e0d270b66197d549ddf
TJ Egan <twegan@us.ibm.com>
2016-10-31 18:21:11 -0500

    responsive stuff

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -8,15 +8,13 @@ const divStyle = {
 
 function Hero() {
   return (
-    <div>
-      <div className="hero" style={divStyle}>
-        <div className="hero__block hero__block--title">
-          <h1 className="hero__title">TJ&nbsp; EGAN</h1>
-        </div>
-        <SocialIcons />
-        <div className="hero__block hero__block--content">
-          <p className="hero__content">Front End Developer + Designer based in <a className="hero__content--link" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Austin,+TX/@30.3079828,-97.8934872,11z/data=!3m1!4b1!4m5!3m4!1s0x8644b599a0cc032f:0x5d9b464bd469d57a!8m2!3d30.2671524!4d-97.74306">Austin, TX</a></p>
-        </div>
+    <div className="hero" style={divStyle}>
+      <div className="hero__block hero__block--title">
+        <h1 className="hero__title">TJ&nbsp; EGAN</h1>
+      </div>
+      <SocialIcons />
+      <div className="hero__block hero__block--content">
+        <p className="hero__content">Front End Developer + Designer based in <a className="hero__content--link" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Austin,+TX/@30.3079828,-97.8934872,11z/data=!3m1!4b1!4m5!3m4!1s0x8644b599a0cc032f:0x5d9b464bd469d57a!8m2!3d30.2671524!4d-97.74306">Austin, TX</a></p>
       </div>
     </div>
   );
--- app/index.jsx
+++ app/index.jsx
@@ -6,14 +6,26 @@ import Blog from './components/blog';
 
 require('./scss/main.scss');
 
-function App() {
-  return (
-    <div className="container">
-      <Hero />
-      <Portfolio />
-      <Blog />
-    </div>
-  );
+export default class App extends React.Component {
+  componentDidMount() {
+    const elem = this.refs.app;
+    elem.style.opacity = 0;
+
+    window.requestAnimationFrame(() => {
+      elem.style.transition = 'opacity 500ms';
+      elem.style.opacity = 1;
+    });
+  }
+
+  render() {
+    return (
+      <div ref="app" className="container">
+        <Hero />
+        <Portfolio />
+        <Blog />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
