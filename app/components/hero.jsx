03fd4672a60d84b03f991339ded8265b230a146b
TJ Egan <twegan@us.ibm.com>
2016-11-21 22:52:54 -0600

    fix spacing

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -16,7 +16,7 @@ function Hero() {
         <SocialIcons />
       </div>
       <div className="hero__footer">
-        <p className="hero__content">Front End Developer + Designer based in <a className="hero__content--link" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Austin,+TX/@30.3079828,-97.8934872,11z/data=!3m1!4b1!4m5!3m4!1s0x8644b599a0cc032f:0x5d9b464bd469d57a!8m2!3d30.2671524!4d-97.74306">Austin, TX</a></p>
+        <p className="hero__content">Front End Developer + Designer based in <a className="hero__content--link" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/Austin,+TX/@30.3079828,-97.8934872,11z/data=!3m1!4b1!4m5!3m4!1s0x8644b599a0cc032f:0x5d9b464bd469d57a!8m2!3d30.2671524!4d-97.74306">Austin, TX</a>.</p>
       </div>
     </div>
   );
